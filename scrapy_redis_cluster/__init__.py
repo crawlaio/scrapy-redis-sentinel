e0a39410e334087ac7e07a038dc98bce5f15212e
施涛 <shitao@zylliondata.com>
2019-08-12 11:59:16 +0800

    scrapy-redis-cluster

---
+++ scrapy_redis_cluster/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from .connection import (
+    get_redis,
+    get_redis_from_settings,
+)
+
+__original_author__ = 'Rolando Espinoza'
+__author__ = 'Shi Tao'
+__email__ = 'shitao0418@gmail.com'
+__version__ = '0.6.8'
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='scrapy-redis-cluster',
+    version=0.1,
+    description="Redis Cluster for Scrapy.",
+    author="Shi Tao",
+    author_email='shitao0418@gmail.com',
+    url='https://github.com/Sitoi/scrapy-redis-cluster.git',
+    packages=find_packages(),
+    install_requires=[
+        'Scrapy>=1.0',
+        'redis>=2.10',
+        'six>=1.5.2',
+        'redis-py-cluster>=1.3.4',
+    ],
+    license="MIT",
+    keywords='scrapy-redis-cluster',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
